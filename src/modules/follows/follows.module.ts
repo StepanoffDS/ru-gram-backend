b0a9f47408571c4c6a54339a1e5a01a585d93f67
Dmitrii <stepanovdimamain@gmail.com>
2026-06-16 23:46:26 +0400

    fixes

--- src/modules/follows/follows.module.ts
+++ src/modules/follows/follows.module.ts
@@ -1,8 +1,10 @@
+import { NotificationsModule } from '@/modules/notifications/notifications.module';
 import { Module } from '@nestjs/common';
 import { FollowsResolver } from './follows.resolver';
 import { FollowsService } from './follows.service';
 
 @Module({
+  imports: [NotificationsModule],
   providers: [FollowsResolver, FollowsService],
   exports: [FollowsService],
 })
